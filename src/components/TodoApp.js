517309faf064ff271283a5246731f9bcc96ab1e7
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-27 11:51:37 -0500

    Final changes

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 function TodoApp() {
 
-    const [title, setTitle] = useState('hola');
+    const [title, setTitle] = useState('Ingrese algo: ');
 
     const handleClick = (e) => {
-        e.preventDefault
+        e.preventDefault();
+        setTitle("se cambió");
+    }
+
+
+    const handleChange = (e) => {
         setTitle(e.target.value);
     }
 
     return (
         <div className="todoContainer">
             <form className='todoCreateForm'>
-                <input className='todoInput' value={title} />
+                <input onChange={handleChange} className='todoInput' value={title} />
                 <input onClick={handleClick} type="submit" value="Create task" className='buttonCreate' />
 
                 {title}
